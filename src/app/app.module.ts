63a220ac427a8c9aa051d700cde27bd54a0db010
oranereuland <orane@reuland.ch>
2017-06-13 16:07:25 +0100

    router and navigation step1
    home page and "page not found" components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
 
+//The Angular Router is an optional service that presents a particular
+//component view for a given URL
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { NavComponent } from './core/nav/nav.component';
+import { HomeComponent } from './core/home/home.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+
+
+const appRoutes: Routes = [
+  //{ path: 'crisis-center', component: CrisisListComponent },
+  //{ path: 'hero/:id',      component: HeroDetailComponent },
+  //{path: 'heroes', component: HeroListComponent, data: { title: 'Heroes List' }},
+  //{ path: '', redirectTo: '/heroes', pathMatch: 'full'},
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PagenotfoundComponent }
+];
+
 
 
 //The @NgModule decorator identifies AppModule as an Angular module class
 @NgModule({
 
   //Specifies a list of directives/pipes that belong to this module. (components)
-  declarations: [AppComponent, FooterComponent, NavComponent],
+  declarations: [AppComponent, FooterComponent, NavComponent, HomeComponent, PagenotfoundComponent],
 
   //Specifies a list of modules whose exported directives/pipes should be
   //available to templates in this module (Modules)
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
 
   //Defines the set of injectable objects that are available in the injector of
   //this module. (services)
